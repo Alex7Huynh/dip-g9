998761a1e9d959a35c6d75075e1a2200468791cf
7hanatos13@gmail.com <7hanatos13@gmail.com@8fd37d7c-63c6-d866-e44a-19fbfe15b7b6>
2014-10-01 02:20:56 +0000

    Update Workshop source code, report, slide.

--- trunk/Workshop/SludgeParticleDetection/SludgeParticleDetection/main.cpp
+++ trunk/Workshop/SludgeParticleDetection/SludgeParticleDetection/main.cpp
@@ -8,22 +8,23 @@ void main()
 	
 	char* filename;
 	filename = new char[100];
-	bool bInput = true;	
+	bool bInput = true;
 
 	/// Load the source image
 	if(bInput) {
-		cout << "Enter image's filename (e.g. F2_P1N.bmp): ";
+		cout << "Enter image's filename (e.g. F1_P3N.bmp): ";
 		cin >> filename;
 		cout << filename;
 	} else {
-		filename = "F4_P1N.bmp";
+		filename = "F1_P3N.bmp";
 	}
 
-	namedWindow( "Original", WINDOW_NORMAL);
-	namedWindow( "Result", WINDOW_NORMAL);
+	namedWindow("Original", WINDOW_NORMAL);
+	namedWindow("Metal detection", WINDOW_NORMAL);
+	namedWindow("Powder detection", WINDOW_NORMAL);
 
 	if (src.empty())
-		src = imread( filename, 1 );
+		src = imread(filename, 1);
 	
 	//Threshold(filename);	// Run threshold demo
 	//Smoothing(filename);	// Run smoothing demo
@@ -37,38 +38,59 @@ void main()
 	/// Enhance contrast: alpha value [1.0-3.0], beta value [0-100]
 	//src.convertTo(dst, -1, 1.2, 50);
 
+	/// Transform with 7 steps
+	//Mat dst = Transform7Steps(src);
+
+
 	/// Convert to grayscale
-	cvtColor( src, src_gray, COLOR_RGB2GRAY );
-	
-	/// Apply Histogram Equalization
-	Mat dst;
-	equalizeHist(src_gray, dst);
-	
-	/// Apply erosion
-	Mat dstErosed;
-	int erosion_size = 3;
-	int erosion_type = MORPH_RECT;
-	Mat element = getStructuringElement( erosion_type, Size( 2*erosion_size + 1, 2*erosion_size+1 ), Point( erosion_size, erosion_size ) );
-	erode( dst, dstErosed, element );
+	Mat src_gray;
+	cvtColor(src, src_gray, COLOR_RGB2GRAY);
+
 
 	/// Apply threshold
 	Mat dstThreshold;
-	threshold( dstErosed, dstThreshold, 80, max_BINARY_value, 1 );
+	threshold( src_gray, dstThreshold, 80, max_BINARY_value, 1 );
 
-	/// Invert color
-	Mat dstInvert;
-	bitwise_not (dst, dstInvert);
+	
+	/// Apply morphology
+	Mat dstErosed;
+	Mat dstDilated;
+	int erosion_size = 5;
+	int erosion_type = MORPH_RECT;
+	Mat element = getStructuringElement( erosion_type, Size( 2*erosion_size + 1, 2*erosion_size+1 ), Point( erosion_size, erosion_size ) );
+	dilate(dstThreshold, dstDilated, element);
+	erode(dstDilated, dstErosed, element);
+	
 
-	/// Canny edge detection
-	Mat dstCanny;
-	dstCanny = CannyThreshold(0, 0, src, dstInvert);
+	/// Apply the metal mask to original image
+	Mat dstMetal;
+	src.copyTo(dstMetal, dstErosed);
 
-	/// Find contours
-	Mat dstContours;
-	dstContours = thresh_callback( 0, 0, dstCanny);
+
+	/// Threshold
+	Mat dstThreshold2;
+	threshold(src_gray, dstThreshold2, 130, max_BINARY_value, 1 );
+
+
+	/// Apply morphology
+	Mat dstDilated2;
+	Mat dstErosed2;	
+	dilate(dstThreshold2, dstDilated2, element);
+	erode(dstDilated2, dstErosed2, element);
+
+
+	/// Subtract metal mask from powder mask
+	Mat dstPowderMask = dstErosed2 - dstErosed;
+
+
+	/// Apply the powder mask to original image
+	Mat dstPowder;
+	src.copyTo(dstPowder, dstPowderMask);
 
     imshow( "Original", src);
-	imshow( "Result", dstCanny);
+	imshow( "Metal detection", dstMetal);
+	imshow( "Powder detection", dstPowder);
+	
 
 	//DisplayCaption( "End: Press a key!", dst);
     waitKey(0);
